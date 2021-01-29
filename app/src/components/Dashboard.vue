8025303aa8ad4fe7b87c68050e8b86629dfe5b27
jarusified <jarus3001@gmail.com>
2021-01-28 22:11:25 -0800

    Add a client

---
+++ app/src/components/Dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script setup>
+import { defineProps, reactive } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+// const state = reactive({ count: 0 })
+</script>
+
+<style scoped>
+</style>
